da09f0f3d2af94684437a53aa297e3392b69c0b9
Kim Dong Hun <hunkim98@gmail.com>
2022-10-15 00:37:10 +0900

    Change folder structure for mocks

---
+++ frontend/src/mocks/data/users.mocks.ts
@@ -0,0 +1,26 @@
+import { User } from "models/users.model";
+
+//this file is for later creating test codes easily
+export const mockUsers: Array<User> = [
+  {
+    id: 1,
+    email: "swpp@snu.ac.kr",
+    nickname: "swpp",
+    password: "iluvswpp",
+    name: "Software Lover",
+  },
+  {
+    id: 2,
+    email: "alan@turing.com",
+    nickname: "turing",
+    password: "iluvswpp",
+    name: "Alan Turing",
+  },
+  {
+    id: 3,
+    email: "edsger@dijkstra.com",
+    nickname: "dijkstra",
+    password: "iluvswpp",
+    name: "Edsger Dijkstra",
+  },
+];
