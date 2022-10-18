a8c7e66dd4385d0666492839880584c8c6523b04
Kim Dong Hun <hunkim98@gmail.com>
2022-10-18 15:52:15 +0900

    Restructure home page components

--- frontend/src/components/FriendListSideBar/styles.ts
+++ frontend/src/components/FriendListSideBar/styles.ts
@@ -1,1 +1,6 @@
 import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 250px;
+  background-color: grey;
+`;
--- frontend/src/containers/HomePage/styles.ts
+++ frontend/src/containers/HomePage/styles.ts
@@ -1,1 +1,7 @@
 import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  height: 100%;
+  flex: 1 0;
+`;
