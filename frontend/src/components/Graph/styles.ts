6ab66aaf10c76357759d0879842cf9487d4c86d5
Kim Dong Hun <hunkim98@gmail.com>
2022-10-18 16:05:02 +0900

    Add background to canvas

---
+++ frontend/src/components/Graph/styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const CanvasContainer = styled.div`
+  background: linear-gradient(
+    180deg,
+    rgba(252, 100, 255, 0.2) 0%,
+    rgba(121, 154, 237, 0.116) 100%
+  );
+  flex: 1 0;
+`;
