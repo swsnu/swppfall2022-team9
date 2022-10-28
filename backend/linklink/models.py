bf510a3cc81c94c746942bcb11a513d3e6080498
notiona <notiona@snu.ac.kr>
2022-10-29 00:33:21 +0900

    backend: Init all models and migrate

--- backend/linklink/models.py
+++ backend/linklink/models.py
@@ -2,6 +2,197 @@
 models module for linklink app
 """
 
-# from django.db import models
+from django.db import models
+from django.conf import settings
 
-# Create your models here.
+from .tags_list import tags
+
+
+#--------------------------------------------------------------------------
+#   LinkLinkUser Related Models
+#--------------------------------------------------------------------------
+
+class LinkLinkUser(models.Model):
+    """
+    LinkLinkUser model class that extends django's User
+    """
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    oneChon = models.ManyToManyField("FriendRequest")
+    nickname = models.CharField(max_length=50)
+    friendRequestToken = models.CharField(max_length=100)
+    emailValidated = models.BooleanField()
+
+    def __str__(self):
+        return f"{self.user.username}"
+
+
+class FriendRequest(models.Model):
+    """
+    FriendRequest model class with 3 status
+    """
+    senderId = models.ForeignKey(
+        LinkLinkUser,
+        on_delete=models.CASCADE,
+        related_name="senderIdFriendRequest", # avoid fields.E304 error
+    )
+    getterId = models.ForeignKey(
+        LinkLinkUser,
+        on_delete=models.CASCADE,
+        related_name="getterIdFriendRequest", # avoid fields.E304 error
+    )
+    FRIEND_REQUEST_STATUS = [
+        ("Pending", "Pending"),
+        ("Accepted", "Accepted"),
+        ("Rejected", "Rejected"),
+    ]
+    status = models.CharField(
+        max_length=8,
+        choices=FRIEND_REQUEST_STATUS,
+        default="Pending"
+    )
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.senderId}->{self.getterId}, status:{self.status}"
+
+
+class Verification(models.Model):
+    """
+    Verification model class
+    """
+    user = models.OneToOneField(LinkLinkUser, on_delete=models.CASCADE)
+    token = models.CharField(max_length=100)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    expiredAt = models.DateTimeField()
+    PURPOSE = [
+        ("Register", "Register"),
+        ("Password", "Password"),
+    ]
+    purpose = models.CharField(
+        max_length=8,
+        choices=PURPOSE,
+        default="Register"
+    )
+
+    def __str__(self):
+        return self.token
+
+
+#--------------------------------------------------------------------------
+#   Profile Related Models
+#--------------------------------------------------------------------------
+
+class SkillTag(models.Model):
+    """
+    SkillTag model class
+    """
+    SKILL_TAGS_LIST = tags.SKILL_TAGS
+    name = models.CharField(
+        max_length=30,
+        choices=[ (skill_tag, skill_tag) for skill_tag in SKILL_TAGS_LIST],
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class QualityTag(models.Model):
+    """
+    QualityTag model class
+    """
+    QUALITY_TAGS_LIST = tags.QUALITY_TAGS
+    name = models.CharField(
+        max_length=30,
+        choices=[
+            (quality_tag, quality_tag) for quality_tag in QUALITY_TAGS_LIST
+        ],
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Profile(models.Model):
+    """
+    Profile model class
+    """
+    user = models.OneToOneField(LinkLinkUser, on_delete=models.CASCADE)
+    skillTags = models.ManyToManyField(SkillTag)
+    qualityTags = models.ManyToManyField(QualityTag)
+    imageUrl = models.CharField(max_length=200)
+    introduction = models.TextField()
+    website = models.CharField(max_length=100, blank=True)
+    GENDERS = [
+        ("M", "Male"),
+        ("F", "Female"),
+        ("X", "None")
+    ]
+    gender = models.CharField(
+        max_length=1,
+        choices=GENDERS,
+        default="None"
+    )
+    birthDate = models.DateField()
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s profile"
+
+
+class Education(models.Model):
+    """
+    Education model class
+    """
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    school = models.CharField(max_length=50)
+    major = models.CharField(max_length=50)
+    dateStart = models.DateField()
+    dateEnd = models.DateField()
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.school}-{self.major}"
+
+
+#--------------------------------------------------------------------------
+#   Chat Related Models
+#--------------------------------------------------------------------------
+
+class ChatLog(models.Model):
+    """
+    ChatLog model class
+    """
+    senderId = models.ForeignKey(
+        LinkLinkUser,
+        on_delete=models.CASCADE,
+        related_name="senderIdChat", # avoid fields.E304 error
+    )
+    getterId = models.ForeignKey(
+        LinkLinkUser,
+        on_delete=models.CASCADE,
+        related_name="getterIdChat", # avoid fields.E304 error
+    )
+    message = models.TextField()
+    createdAt = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.senderId}->{self.getterId}, message:{self.message}"
+
+
+class ChatRoom(models.Model):
+    """
+    ChatRoom model class
+    """
+    users = models.ManyToManyField(LinkLinkUser)
+    chatLogs = models.ManyToManyField(ChatLog)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Chatroom {self.id}"
