9d670c3e4718e5a8295b5676bd37937582f0ccd0
Kim Dong Hun <hunkim98@gmail.com>
2022-10-29 01:00:02 +0900

    Change AuthWrapper test code

--- frontend/src/containers/AuthWrapper/AuthWrapper.test.tsx
+++ frontend/src/containers/AuthWrapper/AuthWrapper.test.tsx
@@ -1,9 +1,8 @@
 import { MemoryRouter, Route, Routes } from "react-router-dom";
-import { fireEvent, screen, waitFor } from "@testing-library/react";
-import axios from "axios";
+import { screen } from "@testing-library/react";
 import { User } from "models/users.model";
 import { renderWithProviders } from "test-utils/mocks";
-import AuthWrapper from "./AuthWrapper";
+import AuthWrapper, { LoginModalMessage } from "./AuthWrapper";
 import { usersStub } from "mocks/stubs/users.stub";
 
 const mockNavigate = jest.fn();
@@ -50,22 +49,11 @@ describe("<AuthWrapper/>", () => {
 
   it("navigates to login page when there is no current user", async () => {
     renderAuthWrapper(null);
-    await waitFor(() => expect(mockNavigate).toHaveBeenCalledWith("/login"));
+    screen.getByText(LoginModalMessage.NOT_AUTHENTICATED);
   });
 
   it("shows logout button as navbar when there is current user", async () => {
     renderAuthWrapper(usersStub[0]);
-    screen.getByRole("button", { name: /logout/i });
-  });
-
-  it("shows logout button as navbar when there is current user", async () => {
-    const spyPutSignOut = jest
-      .spyOn(axios, "put")
-      .mockImplementationOnce(async () => ({}));
-    renderAuthWrapper(usersStub[0]);
-    const logoutButton = screen.getByRole("button", { name: /logout/i });
-    fireEvent.click(logoutButton);
-    //anything that has to do with react state update should be called with waitFor or act!!!
-    await waitFor(() => expect(spyPutSignOut).toHaveBeenCalled());
+    screen.getByText("fake-content");
   });
 });
--- frontend/src/containers/AuthWrapper/AuthWrapper.tsx
+++ frontend/src/containers/AuthWrapper/AuthWrapper.tsx
@@ -7,13 +7,13 @@ interface Props {
   children: React.ReactNode;
 }
 
-enum ModalMessage {
+export enum LoginModalMessage {
   SESSION_EXPIRED = "로그인 세션이 만료되었으니 다시 로그인해주시기 바랍니다 (재로그인하면 현재 보고 계신 창으로 다시 돌아옵니다)",
-  NO_TOKEN = "로그인이 필요한 서비스입니다",
+  NOT_AUTHENTICATED = "로그인이 필요한 서비스입니다",
 }
 
 const AuthWrapper: React.FC<Props> = ({ children }) => {
-  const currentUser = useAppSelector((state) => state.users.currentUser);
+  const currentUser = useAppSelector(state => state.users.currentUser);
 
   return (
     <>
@@ -21,7 +21,9 @@ const AuthWrapper: React.FC<Props> = ({ children }) => {
         <Portal>
           <LoginModal
             message={
-              currentUser ? ModalMessage.SESSION_EXPIRED : ModalMessage.NO_TOKEN
+              currentUser
+                ? LoginModalMessage.SESSION_EXPIRED
+                : LoginModalMessage.NOT_AUTHENTICATED
             }
           />
         </Portal>
