42fa1f07a56a3ff7f7ea0863d2e1f6b5f1a89a47
Kim Dong Hun <hunkim98@gmail.com>
2022-10-29 01:01:20 +0900

    Modify auth wrapper for test

--- frontend/src/containers/AuthWrapper/AuthWrapper.tsx
+++ frontend/src/containers/AuthWrapper/AuthWrapper.tsx
@@ -8,7 +8,6 @@ interface Props {
 }
 
 export enum LoginModalMessage {
-  SESSION_EXPIRED = "로그인 세션이 만료되었으니 다시 로그인해주시기 바랍니다 (재로그인하면 현재 보고 계신 창으로 다시 돌아옵니다)",
   NOT_AUTHENTICATED = "로그인이 필요한 서비스입니다",
 }
 
@@ -19,13 +18,7 @@ const AuthWrapper: React.FC<Props> = ({ children }) => {
     <>
       {!currentUser && (
         <Portal>
-          <LoginModal
-            message={
-              currentUser
-                ? LoginModalMessage.SESSION_EXPIRED
-                : LoginModalMessage.NOT_AUTHENTICATED
-            }
-          />
+          <LoginModal message={LoginModalMessage.NOT_AUTHENTICATED} />
         </Portal>
       )}
       {children}
