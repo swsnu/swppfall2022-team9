56412de8fad4a3fc9fa181210c34405b48d6c265
JadenJSPark <jadenpark98@gmail.com>
2022-11-01 15:18:37 +0900

    FriendListSideBar: initialize

---
+++ frontend/src/components/FriendListSideBar/OneChonListItem.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import * as S from "./styles";
+
+interface Props {}
+
+const OneChonListItem: React.FC<Props> = () => {
+  return <S.Container>I am one chon</S.Container>;
+};
+
+export default OneChonListItem;
