c28b0b7608bb45adbed28c3b405090d6eeb48aa9
JadenJSPark <jadenpark98@gmail.com>
2022-11-01 18:18:10 +0900

    FriendListSideBar: render mock list

--- frontend/src/components/FriendListSideBar/FriendListSideBar.tsx
+++ frontend/src/components/FriendListSideBar/FriendListSideBar.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import * as S from "./styles";
+import OneChonListItem from "./OneChonListItem";
+import TwoChonListItem from "./TwoChonListItem";
 
 interface Props {}
 
 const FriendListSideBar: React.FC<Props> = () => {
-  return <S.Container>Hi there</S.Container>;
+  const oneChonList = [1, 2, 3, 4];
+  const twoChonLIst = [3, 1, 2, 3];
+  return (
+    <S.Container>
+      {oneChonList.map(user => (
+        <OneChonListItem key={user} user={user}></OneChonListItem>
+      ))}
+    </S.Container>
+  );
 };
 
 export default FriendListSideBar;
--- frontend/src/components/FriendListSideBar/styles.ts
+++ frontend/src/components/FriendListSideBar/styles.ts
@@ -1,6 +1,49 @@
 import styled from "styled-components";
-
 export const Container = styled.div`
   min-width: 250px;
-  background-color: grey;
+  max-width:
+  background-color: white;
+  display: flex;
+`;
+
+export const OneChonNode = styled.div<{ url: string }>`
+  display: inline-block;
+  width: 40px;
+  height: 40px;
+  background-image: url(${props => props.url});
+  background-position: 50% 50%;
+  background-size: cover;
+  border-radius: 50%;
+  display: flex;
+`;
+
+export const TwoChonNode = styled.div<{ url: string }>`
+  display: inline-block;
+  width: 40px;
+  height: 40px;
+  background-image: url(${props => props.url});
+  background-position: 50% 50%;
+  background-size: cover;
+  border-radius: 50%;
+  display: flex;
+`;
+
+export const Username = styled.div`
+  display: inline-block;
+  width: 100px;
+  height: 100px;
+  background-position: 50% 50%;
+  background-size: cover;
+  border-radius: 50%;
+  display: flex;
+`;
+
+export const Button = styled.button`
+  display: inline-block;
+  width: 40px;
+  height: 40px;
+  background-position: 50% 50%;
+  background-size: cover;
+  border-radius: 50%;
+  display: flex;
 `;
