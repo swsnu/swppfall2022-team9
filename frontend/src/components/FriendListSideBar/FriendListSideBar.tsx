073dc23e325bba2f57e92042bf72243668a61102
JadenJSPark <jadenpark98@gmail.com>
2022-11-02 03:21:54 +0900

    FriendListSideBar: add buttons

--- frontend/src/components/FriendListSideBar/FriendListSideBar.tsx
+++ frontend/src/components/FriendListSideBar/FriendListSideBar.tsx
@@ -1,17 +1,20 @@
 import React from "react";
 import * as S from "./styles";
 import OneChonListItem from "./OneChonListItem";
-import TwoChonListItem from "./TwoChonListItem";
 
 interface Props {}
 
 const FriendListSideBar: React.FC<Props> = () => {
-  const oneChonList = [1, 2, 3, 4];
-  const twoChonLIst = [3, 1, 2, 3];
+  const oneChonList = [0, 1, 2, 3];
+  const twoChonList = [[5, 6, 7, 8], [9], [10, 11], [12, 13]];
   return (
     <S.Container>
       {oneChonList.map(user => (
-        <OneChonListItem key={user} user={user}></OneChonListItem>
+        <OneChonListItem
+          key={user}
+          user={user}
+          twoChonList={twoChonList[user]}
+        ></OneChonListItem>
       ))}
     </S.Container>
   );
