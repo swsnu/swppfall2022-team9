eb82667349f9fe7d16c949da862caeea0581b1a2
notiona <notiona@snu.ac.kr>
2022-11-02 16:39:21 +0900

    backend:minor: change LinkLinkUser.emailValidated default=False

--- backend/linklink/models.py
+++ backend/linklink/models.py
@@ -23,7 +23,7 @@ class LinkLinkUser(models.Model):
     oneChon = models.ManyToManyField("FriendRequest", blank=True)
     nickname = models.CharField(max_length=50)
     friendRequestToken = models.CharField(max_length=100)
-    emailValidated = models.BooleanField()
+    emailValidated = models.BooleanField(default=False)
 
     def __str__(self):
         return f"{self.user.username}"
