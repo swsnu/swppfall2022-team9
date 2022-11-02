e92f9c62193e5b29596668eae18b31ac7c62813a
notiona <notiona@snu.ac.kr>
2022-11-02 16:38:47 +0900

    backend: implement linklink version of signup

--- backend/linklink/urls.py
+++ backend/linklink/urls.py
@@ -6,7 +6,8 @@ from django.urls import path
 from linklink import views
 
 urlpatterns = [
-     path('test-email/', views.email_test, name='email_test'),
+    path('token/', views.token, name='token'),
+    path('test-email/', views.email_test, name='email_test'),
     path('auth/signup/', views.signup, name='signup'),
     path('auth/signin/', views.signin, name='signin'),
     path('auth/signout/', views.signout, name='signout'),
