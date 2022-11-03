fb262b9943fdf622b3b27c60d8ab73bc84e7bccc
JadenJSPark <jadenpark98@gmail.com>
2022-11-03 18:40:20 +0900

    Revert "FriendListSideBar: add buttons"
    This reverts commit 073dc23e325bba2f57e92042bf72243668a61102.

--- frontend/src/components/FriendListSideBar/styles.ts
+++ frontend/src/components/FriendListSideBar/styles.ts
@@ -1,31 +1,30 @@
 import styled from "styled-components";
 export const Container = styled.div`
   min-width: 250px;
-  max-width: 400px;
+  max-width:
   background-color: white;
+  display: flex;
 `;
 
 export const OneChonNode = styled.div<{ url: string }>`
   display: inline-block;
-  width: 34px;
-  height: 34px;
+  width: 40px;
+  height: 40px;
   background-image: url(${props => props.url});
-  background-position: 50%;
+  background-position: 50% 50%;
   background-size: cover;
   border-radius: 50%;
-  border: 2px solid #000000;
   display: flex;
 `;
 
 export const TwoChonNode = styled.div<{ url: string }>`
   display: inline-block;
-  width: 34px;
-  height: 34px;
+  width: 40px;
+  height: 40px;
   background-image: url(${props => props.url});
-  background-position: 50%;
+  background-position: 50% 50%;
   background-size: cover;
   border-radius: 50%;
-  border: 2px solid #000000;
   display: flex;
 `;
 
@@ -39,31 +38,12 @@ export const Username = styled.div`
   display: flex;
 `;
 
-export const StyledButton = styled.button`
-  display: inline-flex;
-  outline: none;
-  border: none;
-  border-radius: 4px;
-  color: white;
-  width: 100px;
-  height: 100px;
-  font-weight: bold;
-  cursor: pointer;
-  padding-left: 1rem;
-  padding-right: 1rem;
-
-  height: 2.25rem;
-  font-size: 1rem;
-
-  background: #228be6;
-  &:hover {
-    background: #339af0;
-  }
-  &:active {
-    background: #1c7ed6;
-  }
-
-  & + & {
-    margin-left: 1rem;
-  }
+export const Button = styled.button`
+  display: inline-block;
+  width: 40px;
+  height: 40px;
+  background-position: 50% 50%;
+  background-size: cover;
+  border-radius: 50%;
+  display: flex;
 `;
