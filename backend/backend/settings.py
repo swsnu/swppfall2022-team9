3e865cc08ec4bf36fcb5a04c4786b9a4624f42bc
notiona <notiona@snu.ac.kr>
2022-11-03 10:44:47 +0900

    backend: implement register email send

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -134,3 +134,6 @@ EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "linklinkchon@gmail.com"
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+
+# Homepage Base Url
+HOMEPAGE_URL = "http://127.0.0.1:8000/"
--- backend/linklink/views.py
+++ backend/linklink/views.py
@@ -4,6 +4,7 @@ views module for linklink app
 
 import json
 from json.decoder import JSONDecodeError
+import os
 
 from django.conf import settings
 from django.contrib.auth.models import User
@@ -16,35 +17,60 @@ from django.http import (
 )
 from django.template.loader import render_to_string
 from django.views.decorators.csrf import ensure_csrf_cookie
+import requests
 
 from .decorators import allowed_method_or_405, logged_in_or_401
 from .models import LinkLinkUser
 
 
+HOMEPAGE_URL = settings.HOMEPAGE_URL
+EMAIL_HOST_USER = settings.EMAIL_HOST_USER
+
+
 def send_register_email(request, recipient, title, message):
-    subject = title
-    email_from = settings.EMAIL_HOST_USER
-    recipient_list = [recipient]
     context = {
-        "subject": subject,
+        "subject": title,
         "message": message,
-        "button_link": "https://www.google.com",
+        "button_link": HOMEPAGE_URL,
     }
     html_mail = render_to_string("linklink/register_email.html", context)
     send_mail(
-            subject, message, email_from, recipient_list, html_message=html_mail
+        subject=title,
+        message=message,
+        from_email=EMAIL_HOST_USER,
+        recipient_list=[recipient],
+        html_message=html_mail
     )
-    # send_mail(subject, message, email_from, recipient_list)
-    #  return redirect('redirect to a new page')
-    return True
+    #  return redirect("redirect to a new page")
 
 
-def email_test(request):
-    if request.method == "GET":
-        send_register_email(request, 'hunkim98@gmail.com', '이메일 인증' ,'회원가입을 하려면 이메일 인증을 진행해주세요')
-        return HttpResponse(status=201)
+@allowed_method_or_405(["POST"])
+def send_email(request):
+    """
+    Send email to user depending on mode
+    Case1: Register Verify Email
+    Case2: Password Reset Email
+    """
+    try:
+        req_data = json.loads(request.body.decode())
+        mode = req_data["mode"]
+        recipient = req_data["recipient"]
+    except (KeyError, JSONDecodeError) as e:
+        return HttpResponseBadRequest(e) # implicit status code = 400
+    if mode == "register":
+        send_register_email(
+            request,
+            recipient=recipient,
+            title="이메일 인증",
+            message="회원가입을 하려면 이메일 인증을 진행해주세요"
+        )
+    elif mode == "password-reset":
+        pass
     else:
-        return HttpResponseNotAllowed(["POST"])
+        expected_mode = ["register", "password-reset"]
+        raise ValueError(f"Invalid mode. Expected: {expected_mode},\
+            Received: {mode}")
+    return HttpResponse(status=201)
 
 
 @ensure_csrf_cookie
@@ -52,27 +78,28 @@ def token(request):
     """
     Returns csrf token
     """
-    if request.method == 'GET':
+    if request.method == "GET":
         return HttpResponse(status=204)
     else:
-        return HttpResponseNotAllowed(['GET'])
+        return HttpResponseNotAllowed(["GET"])
 
 
-@allowed_method_or_405(['POST'])
+@allowed_method_or_405(["POST"])
 def signup(request):
     """
     When user enters username, password, nickname and requests signup,
     1. django's User object is created
     2. LinkLinkUser object is created, with emailValidated=False
+    3. Send register email to username
     """
     try:
         req_data = json.loads(request.body.decode())
-        username = req_data['username']
-        password = req_data['password']
-        nickname = req_data['nickname']
+        username = req_data["username"]
+        password = req_data["password"]
+        nickname = req_data["nickname"]
     except (KeyError, JSONDecodeError) as e:
-        return HttpResponseBadRequest() # implicit status code = 400
-    # Create django's user
+        return HttpResponseBadRequest(e) # implicit status code = 400
+    # Create django"s user
     user = User.objects.create_user(
         username=username,
         password=password
@@ -83,17 +110,30 @@ def signup(request):
         nickname=nickname,
         emailValidated=False
     )
+    # Send register email to username
+    S = requests.Session()
+    S.get(os.path.join(HOMEPAGE_URL, "api/token/"))
+    token = S.cookies["csrftoken"]
+    send_email_dict = {
+        "mode": "register",
+        "recipient": username
+    }
+    S.post(
+        os.path.join(HOMEPAGE_URL, "api/auth/send_email/"),
+        json.dumps(send_email_dict),
+        headers={"X-CSRFToken": token}
+    )
     return HttpResponse(status=201)
 
 
-@allowed_method_or_405(['POST'])
+@allowed_method_or_405(["POST"])
 def signin(request):
     try:
         req_data = json.loads(request.body.decode())
-        username = req_data['username']
-        password = req_data['password']
+        username = req_data["username"]
+        password = req_data["password"]
     except (KeyError, JSONDecodeError) as e:
-        return HttpResponseBadRequest() # implicit status code = 400
+        return HttpResponseBadRequest(e) # implicit status code = 400
     user = authenticate(username=username, password=password)
     if user is not None: # login successful
         login(request, user) # log the user in, set django session
@@ -102,20 +142,20 @@ def signin(request):
         return HttpResponse(status=401)
 
 
-@allowed_method_or_405(['GET'])
+@allowed_method_or_405(["GET"])
 @logged_in_or_401
 def signout(request):
     logout(request) # log the user out, clear django session
     return HttpResponse(status=204)
 
 
-@allowed_method_or_405(['GET', 'POST', 'DELETE'])
+@allowed_method_or_405(["GET", "POST", "DELETE"])
 @logged_in_or_401
 def onechon(request):
-    if request.method == 'GET':
+    if request.method == "GET":
         pass
-    elif request.method == 'POST':
+    elif request.method == "POST":
         pass
-    elif request.method == 'DELETE':
+    elif request.method == "DELETE":
         pass
     
