df26a0e43b22c04f892e70e4590e8e235199fda1
notiona <notiona@snu.ac.kr>
2022-11-03 10:44:47 +0900

    backend: implement register email send

--- backend/linklink/urls.py
+++ backend/linklink/urls.py
@@ -7,7 +7,7 @@ from linklink import views
 
 urlpatterns = [
     path('token/', views.token, name='token'),
-    path('test-email/', views.email_test, name='email_test'),
+    path('auth/send_email/', views.send_email, name='send_email'),
     path('auth/signup/', views.signup, name='signup'),
     path('auth/signin/', views.signin, name='signin'),
     path('auth/signout/', views.signout, name='signout'),
