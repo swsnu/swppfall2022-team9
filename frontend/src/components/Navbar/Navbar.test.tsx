262aee77d70dac1c15f7b7d94eb5e2604de0b5fb
Kim Dong Hun <hunkim98@gmail.com>
2022-11-05 00:35:28 +0900

    Create basic tests for navbar

--- frontend/src/components/Navbar/Navbar.test.tsx
+++ frontend/src/components/Navbar/Navbar.test.tsx
@@ -1,9 +1,35 @@
-import { render } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import React from "react";
 import { MemoryRouter, Route, Routes } from "react-router-dom";
+import {
+  FriendRequest,
+  FriendRequestStatus,
+} from "server/models/friendRequests.model";
 import { renderWithProviders } from "test-utils/mocks";
 import Navbar from "./Navbar";
 
-const renderNavbar = () => {
+const mockNavigate = jest.fn();
+
+jest.mock("react-router", () => ({
+  // 그래야 NavLink 같은 걸 쓸 수 있다.
+  ...jest.requireActual("react-router"),
+  useNavigate: () => mockNavigate,
+}));
+
+const mockDispatch = jest.fn();
+
+//useDispatch mocking
+jest.mock("react-redux", () => ({
+  ...jest.requireActual("react-redux"),
+  //useDispatch만 우리가 mocking
+  useDispatch: () => mockDispatch,
+}));
+
+const renderNavbar = (
+  friendRequests: Array<
+    FriendRequest & { senderImgUrl: string; senderName: string }
+  >,
+) => {
   renderWithProviders(
     <MemoryRouter>
       <Routes>
@@ -11,7 +37,12 @@ const renderNavbar = () => {
       </Routes>
     </MemoryRouter>,
     {
-      preloadedState: {},
+      preloadedState: {
+        friendRequests: {
+          friendRequests: friendRequests,
+          friendRequestToken: null,
+        },
+      },
     },
   );
 };
@@ -21,7 +52,57 @@ describe("<Navbar/>", () => {
     jest.clearAllMocks();
   });
 
-  it("renders Navbar", async () => {
-    renderNavbar();
+  it("renders Navbar with no friend request", async () => {
+    renderNavbar([]);
+  });
+
+  it("clicks navigations and etc", async () => {
+    renderNavbar([]);
+    const accountButton = screen.getByRole("account");
+    fireEvent.click(accountButton);
+    expect(mockNavigate).toHaveBeenCalled();
+    const chatsButton = screen.getByRole("chats");
+    fireEvent.click(chatsButton);
+    expect(mockNavigate).toHaveBeenCalled();
+    const logoImage = screen.getByRole("logo");
+    fireEvent.click(logoImage);
+    expect(mockNavigate).toHaveBeenCalled();
+    const searchButton = screen.getByRole("search");
+    // TODO: search click should render a search bar in the canvas!!
+    fireEvent.click(searchButton);
+  });
+
+  it("clicks notification", async () => {
+    renderNavbar([
+      {
+        id: 1,
+        senderId: 1,
+        getterId: 2,
+        status: FriendRequestStatus.PENDING,
+        senderImgUrl: "test",
+        senderName: "test",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+    const notificationButton = screen.getByRole("notification");
+    fireEvent.click(notificationButton);
+    const acceptButton = await waitFor(() => screen.getByText("수락"));
+    const rejectButton = await waitFor(() => screen.getByText("거절"));
+    fireEvent.click(acceptButton);
+    expect(mockDispatch).toHaveBeenCalled();
+    fireEvent.click(rejectButton);
+    expect(mockDispatch).toHaveBeenCalled();
+    // click somewhere else
+    const accountButton = screen.getByRole("account");
+    fireEvent.click(accountButton);
+  });
+
+  it("test useRef", async () => {
+    const divElement = document.createElement("div");
+    jest.spyOn(React, "useRef").mockReturnValue({
+      current: divElement,
+    });
+    renderNavbar([]);
   });
 });
