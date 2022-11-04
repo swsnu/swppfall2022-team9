2512622d2d0a02e88def7750ae7268eab3ceb872
JadenJSPark <jadenpark98@gmail.com>
2022-11-04 20:27:28 +0900

    CreateProfile: add profile dtos

--- frontend/src/server/dto/profile/profile.dto.ts
+++ frontend/src/server/dto/profile/profile.dto.ts
@@ -1,1 +1,12 @@
-export type GetUserDto = number;
+import { Profile } from "models/profile.model";
+
+export type GetProfileDto = Profile;
+
+// create profile
+export type PostCreateProfileDto = Profile;
+
+// change profile
+export type PostEditProfileDto = Profile;
+
+// When someone evalutes the user's qualities
+export type PostUpdateProfileDto = Profile;
