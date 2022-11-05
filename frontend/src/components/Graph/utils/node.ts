a32c40f6e8f631cc7390a52b1361bd59cfe5ac9c
dongjoocha <dongjoocha@naver.com>
2022-11-05 19:05:41 +0900

    Add HTMLImageElement to UserNode

--- frontend/src/components/Graph/utils/node.ts
+++ frontend/src/components/Graph/utils/node.ts
@@ -1,17 +1,32 @@
 import { Coord } from "types/canvas.types";
 
 export class UserNode {
-  radius = 50;
+  radius = 40;
 
-  imgUrl: string;
+  imgElement: HTMLImageElement;
 
   name: string;
 
   coord: Coord;
 
   constructor(imgUrl: string, name: string, coord: Coord) {
-    this.imgUrl = imgUrl;
+    this.imgElement = new Image();
+    this.imgElement.src = imgUrl;
     this.name = name;
     this.coord = coord;
+  }
+}
+
+export class OneChonNode extends UserNode {
+  twoChonNodes?: UserNode[];
+
+  constructor(
+    imgUrl: string,
+    name: string,
+    coord: Coord,
+    twoChonNodes: UserNode[],
+  ) {
+    super(imgUrl, name, coord);
+    this.twoChonNodes = twoChonNodes;
   }
 }
