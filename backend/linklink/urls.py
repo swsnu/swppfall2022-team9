a74958f2126f1a869067de48f6092c449b62f8f6
notiona <notiona@snu.ac.kr>
2022-11-07 21:46:04 +0900

    backend: implement verify GET API

--- backend/linklink/urls.py
+++ backend/linklink/urls.py
@@ -6,9 +6,10 @@ from django.urls import path
 from linklink import views
 
 urlpatterns = [
-    path('token/', views.token, name='token'),
+    path('token/', views.csrf_token, name='csrf_token'),
     path('auth/signup/', views.signup, name='signup'),
     path('auth/signin/', views.signin, name='signin'),
     path('auth/signout/', views.signout, name='signout'),
+    path('auth/verify/<str:token>/', views.verify, name='verify'),
     path('user/onechon/', views.onechon, name='onechon'),
 ]
