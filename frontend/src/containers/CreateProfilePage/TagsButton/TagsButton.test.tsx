5d4f5031c2ba420c637917f0fcd85f27ec968575
JadenJSPark <jadenpark98@gmail.com>
2022-11-09 20:44:22 +0900

    Test: TagsButton test

--- frontend/src/containers/CreateProfilePage/TagsButton/TagsButton.test.tsx
+++ frontend/src/containers/CreateProfilePage/TagsButton/TagsButton.test.tsx
@@ -1,14 +1,30 @@
-import { render } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import TagsButton from "./TagsButton";
+
+const mockUseState = jest.fn();
+
+jest.mock("react-redux", () => ({
+  ...jest.requireActual("react-redux");
+  useState: () => mockUseState,
+}));
 
 describe("<TagsButton/>", () => {
   beforeEach(() => {
     jest.clearAllMocks();
   });
 
-  it("renders Tags", async () => {
+  it("renders TagsButton", async () => {
     render(
       <TagsButton tagName="Test" setProfile={() => {}} propsName="test" />,
     );
+  });
+
+  it("clicks tags button", async () => {
+    render(
+      <TagsButton tagName="Test" setProfile={() => {}} propsName="test" />,
+    );
+    const button = screen.getByRole("button");
+    fireEvent.click(button);
+    expect(mockUseState).toHaveBeenCalled()
   });
 });
