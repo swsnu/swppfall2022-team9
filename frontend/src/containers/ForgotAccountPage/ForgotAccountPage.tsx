a690c6cc06d5b0db68156024046183e57a79b32c
Kim Dong Hun <hunkim98@gmail.com>
2022-11-09 13:09:07 +0900

    Create forgot account page structure

--- frontend/src/containers/ForgotAccountPage/ForgotAccountPage.tsx
+++ frontend/src/containers/ForgotAccountPage/ForgotAccountPage.tsx
@@ -1,9 +1,51 @@
 import React from "react";
-import * as S from "./styles";
-interface Props {}
+import * as FormStyles from "styles/common.form.styles";
 
-const ForgotAccountPage: React.FC<Props> = () => {
-  return <S.Container>ForgotAccountPage</S.Container>;
+const ForgotAccountPage: React.FC = () => {
+  const onSubmit = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+  };
+  return (
+    <FormStyles.Container>
+      <FormStyles.FormContainer>
+        <FormStyles.Header>
+          <FormStyles.HeaderText>아아디/비밀번호 찾기</FormStyles.HeaderText>
+        </FormStyles.Header>
+        <FormStyles.Form onSubmit={onSubmit}>
+          <FormStyles.Label>
+            <FormStyles.LabelText>현재 비밀번호</FormStyles.LabelText>
+            <FormStyles.InputContainer>
+              <FormStyles.Input />
+            </FormStyles.InputContainer>
+          </FormStyles.Label>
+          <FormStyles.Label>
+            <FormStyles.LabelText>새로운 비밀번호</FormStyles.LabelText>
+            <FormStyles.InputContainer>
+              <FormStyles.Input />
+            </FormStyles.InputContainer>
+          </FormStyles.Label>
+          <FormStyles.Label>
+            <FormStyles.LabelText>비밀번호 확인</FormStyles.LabelText>
+            <FormStyles.InputContainer>
+              <FormStyles.Input />
+            </FormStyles.InputContainer>
+          </FormStyles.Label>
+          <FormStyles.FormInnerButton
+            type="submit"
+            onClick={onSubmit}
+            style={{
+              marginTop: 20,
+              width: "100%",
+              maxWidth: 340,
+              fontWeight: "bold",
+            }}
+          >
+            비밀반호 변경
+          </FormStyles.FormInnerButton>
+        </FormStyles.Form>
+      </FormStyles.FormContainer>
+    </FormStyles.Container>
+  );
 };
 
 export default ForgotAccountPage;
