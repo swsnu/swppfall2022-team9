14a1fbc8afb427b92f1fb418824d763606b88eeb
JadenJSPark <jadenpark98@gmail.com>
2022-11-10 00:42:57 +0900

    test: chonlistitem

--- frontend/src/components/FriendListSideBar/ChonListItem/ChonListItem.test.tsx
+++ frontend/src/components/FriendListSideBar/ChonListItem/ChonListItem.test.tsx
@@ -1,5 +1,36 @@
-import { render } from "@testing-library/react";
-import OneChonListItem from "./ChonListItem";
+import { fireEvent, screen } from "@testing-library/react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { renderWithProviders } from "test-utils/mocks";
+import ChonListItem from "./ChonListItem";
+
+const mockNavigate = jest.fn();
+
+jest.mock("react-router", () => ({
+  ...jest.requireActual("react-router"),
+  useNavigate: () => mockNavigate,
+}));
+
+const renderChonListItem = () => {
+  renderWithProviders(
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <ChonListItem
+              userId={1}
+              firstname="test"
+              lastname="test"
+              imgUrl="testurl.com"
+              twoChonList={[1, 2, 3]}
+              isTwoChon={false}
+            />
+          }
+        />
+      </Routes>
+    </BrowserRouter>,
+  );
+};
 
 describe("<OneChonListItem/>", () => {
   beforeEach(() => {
@@ -7,15 +38,18 @@ describe("<OneChonListItem/>", () => {
   });
 
   it("renders OneChonListItem", async () => {
-    render(
-      <OneChonListItem
-        userId={1}
-        firstname="test"
-        lastname="test"
-        imgUrl="testurl.com"
-        twoChonList={null}
-        isTwoChon={false}
-      />,
-    );
+    renderChonListItem();
+  });
+
+  it("clicks on usernode", async () => {
+    renderChonListItem();
+    const userNode = screen.getByRole("NodeClick");
+    fireEvent.click(userNode);
+  });
+
+  it("clicks on toggle button", async () => {
+    renderChonListItem();
+    const button = screen.getByRole("button");
+    fireEvent.click(button);
   });
 });
