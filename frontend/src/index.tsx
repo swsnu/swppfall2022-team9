7c94558d81fa65500829401374c68d431394c483
Kim Dong Hun <hunkim98@gmail.com>
2022-11-09 13:15:01 +0900

    Add csrf setup for frontend

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,6 +5,10 @@ import { Provider } from "react-redux";
 import store from "store";
 import App from "App";
 import { AlertContextProvider } from "containers/Context/AlertContext/AlertContext";
+import axios from "axios";
+
+// this sets our csrf token
+axios.get("/api/csrf_token/");
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
