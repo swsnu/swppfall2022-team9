0b36e0d65b6419f4720a79084ee2e8b7b9bc5be8
JadenJSPark <jadenpark98@gmail.com>
2022-11-10 14:42:05 +0900

    test: mock in progress

---
+++ frontend/src/server/mock/profile.ts
@@ -0,0 +1,34 @@
+// IMPORTANT: everything related to dto, models, stubs should be relative imports
+// import {
+//   GetChonListDto,
+//   PostSignInDto,
+//   PostSignUpDto,
+// } from "../dto/users/users.dto";
+import {
+  GetChonListResDto,
+  // PostSignInResDto,
+  // PostSignUpResDto,
+} from "../dto/users/users.res.dto";
+// IMPORTANT: everything related to dto, models, stubs should be relative imports
+import { PostCreateProfileDto } from "../dto/profile/profile.dto";
+
+import { Application } from "express";
+
+import low from "lowdb";
+import { Schema } from ".";
+
+export default function applyProfileApi(
+  server: Application,
+  db: low.LowdbSync<Schema>,
+) {
+  server.post<unknown, unknown, PostCreateProfileDto>(
+    "/api/profile/",
+    async req => {
+      const profiles = db.get("profiles").value();
+      await db
+        .get("profiles")
+        .push({ ...req.body })
+        .write();
+    },
+  );
+}
