af760333e731ce06e4df1cff6ba0735a1204e9c3
JadenJSPark <jadenpark98@gmail.com>
2022-11-10 23:56:09 +0900

    test: start slice testing

---
+++ frontend/src/store/slices/profile.test.ts
@@ -0,0 +1,9 @@
+import { render } from "@testing-library/react";
+import { postCreateProfile } from "./profile";
+describe("profile reducer", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("renders profile page", async () => {});
+});
