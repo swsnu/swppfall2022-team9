10e281079e9895f35b2582b84263e51f8aa2f8d3
notiona <notiona@snu.ac.kr>
2022-11-12 03:26:21 +0900

    backend: automate skillTags & qualityTags init db load

---
+++ backend/linklink/fixtures/skill_tag_generator.py
@@ -0,0 +1,23 @@
+"""
+This module generates a init_fake_skill_tags.json file
+based on tags_list/tags.py
+"""
+
+import json
+
+import tags_list.tags as tags
+
+if __name__=="__main__":
+    skill_tags = tags.SKILL_TAGS
+    skill_tags_json_fixture = []
+    for idx, skill_tag in enumerate(skill_tags):
+        skill_tag_json = {
+            "model": "linklink.skilltag",
+            "pk": idx+1,
+            "fields": {
+                "name": skill_tag
+            }
+        }
+        skill_tags_json_fixture.append(skill_tag_json)
+    with open("init_fake_skill_tags.json", "w") as json_file:
+        json.dump(skill_tags_json_fixture, json_file, indent=4)
