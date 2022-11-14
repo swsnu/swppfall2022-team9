c2044ccf301418f54c1d771d3020db457063d35b
notiona <notiona@snu.ac.kr>
2022-11-15 03:09:11 +0900

    backend: setup GET other_profile view, url

--- backend/linklink/urls.py
+++ backend/linklink/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
     path('auth/signout/', views.signout, name='signout'),
     path('auth/verify/<str:token>/', views.verify, name='verify'),
     path('user/friend/', views.friend, name='friend'),
-    path('profile/', views.profile, name='profile'),
+    path('profile/', views.my_profile, name='my_profile'),
+    path('profile/<int:user_id>/', views.other_profile, name='other_profile'),
 ]
