3f3d650e760bb95960e2be7d72adf4da8ce056b4
JadenJSPark <jadenpark98@gmail.com>
2022-11-15 16:06:06 +0900

    test: testing done except useParams

--- frontend/src/containers/EvaluateQualityPage/EvaluateQualityPage.test.tsx
+++ frontend/src/containers/EvaluateQualityPage/EvaluateQualityPage.test.tsx
@@ -3,7 +3,7 @@ import { renderWithProviders } from "test-utils/mocks";
 import EvaluateQualityPage from "./EvaluateQualityPage";
 import { MemoryRouter, Route, Routes, Navigate } from "react-router-dom";
 import axios from "axios";
-import { fireEvent, screen, render } from "@testing-library/react";
+import { fireEvent, screen, render, waitFor } from "@testing-library/react";
 
 const mockNavigate = jest.fn();
 
@@ -19,46 +19,51 @@ jest.mock("react-redux", () => ({
   useDispatch: () => mockDispatch,
 }));
 
-const renderEvaluateQualityPage = (alertProviderProps?: AlertContextProps) => {
-  renderWithProviders(
+const renderEvaluateQualityPage = () => {
+  return renderWithProviders(
     <MemoryRouter>
       <Routes>
-        <Route path="/" element={<EvaluateQualityPage />} />
-        {/* <Route path="*" element={<Navigate to="/1" />} /> */}
+        <Route path="/:id" element={<EvaluateQualityPage />} />
+        <Route path="*" element={<Navigate to="/1" />} />
       </Routes>
     </MemoryRouter>,
     {
       preloadedState: {},
     },
-    alertProviderProps,
   );
 };
 
 describe("<EvaluateQualityPage/>", () => {
-  let alertProviderProps: AlertContextProps;
   beforeEach(() => {
     jest.clearAllMocks();
-    alertProviderProps = {
-      open: jest.fn(),
-      close: jest.fn(),
-    };
-    // jest.mock("react-router-dom", () => ({
-    //   ...jest.requireActual("react-router-dom"),
-    //   useParams: () => ({
-    //     connectionId: "12",
-    //   }),
-    // }));
   });
 
   it("renders EvaluateQualityPage", async () => {
-    renderEvaluateQualityPage(alertProviderProps);
+    renderEvaluateQualityPage();
   });
 
-  it("renders EvaluateQualityPage", async () => {
-    renderEvaluateQualityPage(alertProviderProps);
-    axios.get = jest.fn().mockResolvedValue({
-      data: [{ name: "tag1" }, { name: "tag2" }, { name: "tag3" }],
-    });
-    screen.debug();
+  it("tests add and submit button and delete", async () => {
+    renderEvaluateQualityPage();
+    const input = screen.getByRole("combobox");
+    fireEvent.change(input, { target: { value: "sincere" } });
+    const select = screen.getByText("Sincere");
+    fireEvent.click(select);
+    const button = screen.getAllByRole("button");
+
+    fireEvent.click(button[0]);
+    fireEvent.click(button[1]);
+
+    const close = screen.getByRole("close-icon");
+    fireEvent.click(close);
+  });
+
+  it("tests useParams", async () => {
+    jest.mock("react-router-dom", () => ({
+      ...jest.requireActual("react-router-dom"),
+      useParams: () => ({
+        userId: 1,
+      }),
+    }));
+    renderEvaluateQualityPage();
   });
 });
