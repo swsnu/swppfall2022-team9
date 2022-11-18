b0a34dd9e27b6a5d142c818674123cb4ca13ddab
dongjoocha <dongjoocha@naver.com>
2022-11-18 13:04:02 +0900

    Implement search slice

--- frontend/src/store/slices/index.ts
+++ frontend/src/store/slices/index.ts
@@ -7,11 +7,13 @@ import userSlice from "./users";
 import { RootState } from "..";
 import friendRequestSlice from "./friendRequests";
 import profileSlice from "./profile";
+import searchSlice from "./search";
 
 const rootReducer = combineReducers({
   users: userSlice,
   friendRequests: friendRequestSlice,
   profile: profileSlice,
+  search: searchSlice,
 });
 
 export const setupStore = (preloadedState?: PreloadedState<RootState>) => {
