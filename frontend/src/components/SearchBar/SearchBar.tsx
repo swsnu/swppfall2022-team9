9819227a741ae77fd6cdc58800ae98b477767b1f
dongjoocha <dongjoocha@naver.com>
2022-11-18 13:04:24 +0900

    Implement search bar

---
+++ frontend/src/components/SearchBar/SearchBar.tsx
@@ -0,0 +1,38 @@
+import { BsSearch } from "react-icons/bs";
+import { useAppDispatch, useAppSelector } from "store/hooks";
+import { searchActions } from "store/slices/search";
+import * as S from "./styles";
+
+interface Props {}
+
+const SearchBar: React.FC<Props> = () => {
+  const dispatch = useAppDispatch();
+  const users = useAppSelector(state => state.users);
+  const friendList = users.friendList;
+
+  return (
+    <S.Container>
+      <BsSearch
+        role="search-icon"
+        aria-label="search-icon"
+        style={{ marginLeft: 15 }}
+        size={30}
+      />
+      <S.Input
+        type="text"
+        onChange={e => {
+          dispatch(
+            searchActions.search({
+              searchWord: e.target.value,
+              friendList: friendList,
+            }),
+          );
+        }}
+        placeholder={"검색어를 입력하세요"}
+        maxLength={26}
+      />
+    </S.Container>
+  );
+};
+
+export default SearchBar;
---
+++ frontend/src/components/SearchBar/styles.ts
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  box-sizing: border-box;
+  position: absolute;
+  width: 568px;
+  height: 56px;
+  left: 43%;
+  top: 10%;
+  background: #ffffff;
+  border: 1px solid #000000;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 15px;
+  display: flex;
+  align-items: center;
+`;
+
+export const Input = styled.input`
+  border: none;
+  padding: 0 4px;
+  font-size: 1.3rem;
+  font-weight: bold;
+  margin: 0 2%;
+  width: 100%;
+
+  :focus {
+    outline: none;
+  }
+`;
