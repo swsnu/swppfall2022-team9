cfc1c5d4206f7fcd927a8ea2326c38648acdfcf0
notiona <notiona@snu.ac.kr>
2022-11-22 23:18:46 +0900

    backend: implement PUT /api/friendRequest/:friendRequestId/

--- backend/linklink/views.py
+++ backend/linklink/views.py
@@ -170,6 +170,30 @@ def profile_package_response_body(profile_found, is_me):
     response_dict["imgUrl"] = profile_found.imgUrl
     return response_dict
 
+
+def is_within_twochon(linklinkuser1, linklinkuser2):
+    """
+    Helper function to determine whether two users are within twochon.
+    """
+    # Get all Accepted FriendRequest
+    all_accepted_friend_requests = FriendRequest.objects.filter(
+        status="Accepted"
+    )
+    # Get onechon of linklinkuser1
+    onechon_list = get_onechon_linklinkuser_list(
+        all_accepted_friend_requests,
+        linklinkuser1
+    )
+    within_twochon_list = onechon_list.copy()
+    for onechon_linklinkuser in onechon_list:
+        twochon_list = get_onechon_linklinkuser_list(
+            all_accepted_friend_requests,
+            onechon_linklinkuser,
+            exclude_linklinkuser=linklinkuser1
+        )
+        within_twochon_list.extend(twochon_list)
+    return linklinkuser2 in within_twochon_list
+
 #--------------------------------------------------------------------------
 #   Auth Related APIs
 #--------------------------------------------------------------------------
@@ -612,6 +636,15 @@ def friend_request(request):
                 data={"message":friend_request_already_exist_message}
             )
         else:
+            # Check whether two users are within twochon
+            other_user = LinkLinkUser.objects.get(id=getter_id)
+            if not is_within_twochon(linklinkuser, other_user):
+                not_within_twochon_message = \
+                    "Two users are not within twochon."
+                return JsonResponse(
+                    status=403,
+                    data={"message": not_within_twochon_message},
+                )
             # Create FriendRequest to getterId
             FriendRequest.objects.create(
                 senderId=linklinkuser_sender,
@@ -623,6 +656,131 @@ def friend_request(request):
 
 @allowed_method_or_405(["PUT"])
 @logged_in_or_401
-def friend_request_respond(request):
+def friend_request_respond(request, friend_request_id):
     if request.method == "PUT":
-        pass
+        # 1. Get FriendRequest=friendRequestId
+        # 2. Check write permission of current user
+        # 3. Change status of FriendRequest
+        try:
+            req_data = json.loads(request.body.decode())
+            new_status = req_data["status"]
+        except (KeyError, JSONDecodeError) as e:
+            return HttpResponseBadRequest(e) # implicit status code = 400
+        # Get FriendRequest=friendRequestId
+        try:
+            friend_request_found = FriendRequest.objects.get(id=friend_request_id)
+        except FriendRequest.DoesNotExist:
+            friend_request_not_found_message = \
+                f"FriendRequest id={friend_request_id} not found."
+            return JsonResponse(
+                status=404,
+                data={"message": friend_request_not_found_message}
+            )
+        # Check write permission  and state transition
+        # State Transition Table
+        #--------------------------------------------------|
+        # Before\After | Accepted  | Pending   | Rejected  |
+        # Accepted     | No change | Illegal   | Both      |     
+        # Pending      | Getter    | No change | Getter    |
+        # Rejected     | Illegal   | Both*     | No change |
+        #--------------------------------------------------|
+        # No change: No state change, do nothing 
+        # Illegal: State Change not allowed
+        # Getter: Only Getter has write permission
+        # Both: Both Sender and Getter have write permission
+        # Both*: Both Sender and Getter have write permission,
+        #   with some additional conditions
+        #   New request should reset sender and getter accordingly
+        #   New request can be made only if the two LinkLinkUsers
+        #       are within twochon relationship
+        state_transition_dict = {
+            "Accepted": {
+                "Accepted": "No change",
+                "Pending": "Illegal",
+                "Rejected": "Both",
+            },
+            "Pending": {
+                "Accepted": "Getter",
+                "Pending": "No change",
+                "Rejected": "Getter",
+            },
+            "Rejected": {
+                "Accepted": "Illegal",
+                "Pending": "Both",
+                "Rejected": "No change",
+            },
+        }
+        linklinkuser = request.user.linklinkuser
+        prev_status = friend_request_found.status
+        state_transition_result = state_transition_dict[prev_status][new_status]
+        if state_transition_result == "Illegal":
+            illegal_state_transition_message = (
+                "State Transition: "
+                f"{prev_status}->{new_status} not allowed."
+            )
+            return JsonResponse(
+                status=403,
+                data={"message": illegal_state_transition_message},
+            )
+        elif state_transition_result == "No Change":
+            return JsonResponse(
+                status=200,
+                data={
+                "id": friend_request_found.id,
+                "senderId": friend_request_found.senderId.id,
+                "getterId": friend_request_found.getterId.id,
+                "status": friend_request_found.status,
+                }
+            )
+        elif state_transition_result == "Getter":
+            if friend_request_found.getterId.id != linklinkuser.id:
+                no_write_permission_message = (
+                    "Current user has no write permission for "
+                    f"FriendRequest id={friend_request_id}."
+                )
+                return JsonResponse(
+                    status=403,
+                    data={"message": no_write_permission_message}
+                )
+        elif state_transition_result == "Both":
+            if friend_request_found.senderId.id != linklinkuser.id and \
+                friend_request_found.getterId.id != linklinkuser.id:
+                no_write_permission_message = (
+                    "Current user has no write permission for "
+                    f"FriendRequest id={friend_request_id}."
+                )
+                return JsonResponse(
+                    status=403,
+                    data={"message": no_write_permission_message}
+                )
+            else:
+                if prev_status == "Rejected": # Both* Case
+                    sender_getter = [
+                        friend_request_found.senderId,
+                        friend_request_found.getterId
+                    ]
+                    sender_getter.remove(linklinkuser)
+                    other_user = sender_getter[0]
+                    # Check whether two users are within twochon
+                    if not is_within_twochon(linklinkuser, other_user):
+                        not_within_twochon_message = \
+                            "Two users are not within twochon."
+                        return JsonResponse(
+                            status=403,
+                            data={"message": not_within_twochon_message},
+                        )
+                    # set new sender getter
+                    friend_request_found.senderId = linklinkuser
+                    friend_request_found.getterId = other_user
+        # Change status of FriendRequest
+        friend_request_found.status = new_status
+        friend_request_found.save()
+        return JsonResponse(
+            status=200,
+            data={
+                "id": friend_request_found.id,
+                "senderId": friend_request_found.senderId.id,
+                "getterId": friend_request_found.getterId.id,
+                "status": friend_request_found.status,
+            }
+        )
