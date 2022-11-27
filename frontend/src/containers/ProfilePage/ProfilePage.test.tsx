382a85c881a3a163a2066dec227b49ef2a0303a4
dongjoocha <dongjoocha@naver.com>
2022-11-27 16:56:46 +0900

    Implement profile page test

--- frontend/src/containers/ProfilePage/ProfilePage.test.tsx
+++ frontend/src/containers/ProfilePage/ProfilePage.test.tsx
@@ -1,5 +1,11 @@
 import { renderWithProviders } from "test-utils/mocks";
-import { waitFor, screen, fireEvent, render } from "@testing-library/react";
+import {
+  waitFor,
+  screen,
+  fireEvent,
+  render,
+  act,
+} from "@testing-library/react";
 import { MemoryRouter, Route, Routes, Navigate } from "react-router-dom";
 import ProfilePage from "./ProfilePage";
 import { User } from "server/models/users.model";
@@ -11,9 +17,10 @@ import {
   AlertContextProvider,
 } from "containers/Context/AlertContext/AlertContext";
 import { profileStub } from "server/stubs/profiles.stub";
-import { profilePageUsersStub } from "server/stubs/users.stub";
+import { friendListStub, usersStub } from "server/stubs/users.stub";
 import { Provider } from "react-redux";
 import store from "store";
+import { friendRequestsStub } from "server/stubs/friendRequests.stub";
 
 const mockNavigate = jest.fn();
 jest.mock("react-router", () => ({
@@ -31,13 +38,14 @@ const renderProfilePage = (
   currentUser: User | null,
   friendList: OneChonInfo[],
   currentProfile: (Profile & { qualityTags: QualityTags }) | null,
+  userId: number,
   alertProviderProps?: AlertContextProps,
 ) => {
   return renderWithProviders(
     <MemoryRouter>
       <Routes>
         <Route path="/:userId" element={<ProfilePage />} />
-        <Route path="*" element={<Navigate to="/1" />} />
+        <Route path="*" element={<Navigate to={`/${userId}`} />} />
       </Routes>
     </MemoryRouter>,
     {
@@ -48,6 +56,13 @@ const renderProfilePage = (
         },
         profile: {
           currentProfile,
+        },
+        friendRequests: {
+          friendRequestToken: null,
+          friendRequests: [
+            { ...friendRequestsStub[0], senderImgUrl: "", senderName: "" },
+            { ...friendRequestsStub[5], senderImgUrl: "", senderName: "" },
+          ],
         },
       },
     },
@@ -60,95 +75,6 @@ const currentProfileStub = {
   qualityTags: [{ name: "t1" }, { name: "t2" }, { name: "t1" }],
 };
 
-const friendListStub = [
-  {
-    id: 1,
-    firstname: "hi",
-    lastname: "hi",
-    imgUrl: "https://naver.com",
-    chons: [
-      { id: 2, firstname: "hi", lastname: "hi", imgUrl: "https://naver.com" },
-      { id: 3, firstname: "hi", lastname: "hi", imgUrl: "https://naver.com" },
-    ],
-  },
-];
-
-const friendListStub2 = [
-  {
-    id: 2,
-    firstname: "hi",
-    lastname: "hi",
-    imgUrl: "https://naver.com",
-    chons: [
-      { id: 3, firstname: "hi", lastname: "hi", imgUrl: "https://naver.com" },
-      { id: 4, firstname: "hi", lastname: "hi", imgUrl: "https://naver.com" },
-    ],
-  },
-  {
-    id: 5,
-    firstname: "hhi",
-    lastname: "hhi",
-    imgUrl: "https://naver.com",
-    chons: [
-      {
-        id: 3,
-        firstname: "asdfas",
-        lastname: "adsf",
-        imgUrl: "https://naver.com",
-      },
-      {
-        id: 1,
-        firstname: "asdf",
-        lastname: "adsf",
-        imgUrl: "https://naver.com",
-      },
-    ],
-  },
-];
-
-const friendListStub3 = [
-  {
-    id: 2,
-    firstname: "hi",
-    lastname: "hi",
-    imgUrl: "https://naver.com",
-    chons: [
-      { id: 3, firstname: "hi", lastname: "hi", imgUrl: "https://naver.com" },
-      { id: 4, firstname: "hi", lastname: "hi", imgUrl: "https://naver.com" },
-    ],
-  },
-  {
-    id: 5,
-    firstname: "hhi",
-    lastname: "hhi",
-    imgUrl: "https://naver.com",
-    chons: [
-      {
-        id: 3,
-        firstname: "asdfas",
-        lastname: "adsf",
-        imgUrl: "https://naver.com",
-      },
-      {
-        id: 4,
-        firstname: "asdf",
-        lastname: "adsf",
-        imgUrl: "https://naver.com",
-      },
-    ],
-  },
-];
-
-const friendListStub4 = [
-  {
-    id: 5,
-    firstname: "hi",
-    lastname: "hi",
-    imgUrl: "https://naver.com",
-    chons: [],
-  },
-];
-
 describe("<ProfilePage/>", () => {
   let alertProviderProps: AlertContextProps;
   beforeEach(() => {
@@ -157,73 +83,75 @@ describe("<ProfilePage/>", () => {
       open: jest.fn(),
       close: jest.fn(),
     };
+    mockDispatch.mockReturnValue({
+      unwrap: () => ({ ...profileStub }),
+    });
   });
 
   it("renders profile page", async () => {
-    renderProfilePage(null, [], null, alertProviderProps);
-  });
-
-  it("renders profile page with data", async () => {
-    renderProfilePage(
-      profilePageUsersStub[0],
-      friendListStub,
-      currentProfileStub,
-      alertProviderProps,
-    );
-  });
-
-  it("tests useEffect branchign with different oneChon id", async () => {
-    renderProfilePage(
-      profilePageUsersStub[0],
-      friendListStub4,
-      currentProfileStub,
-      alertProviderProps,
-    );
+    await act(async () => {
+      renderProfilePage(
+        usersStub[0],
+        friendListStub,
+        null,
+        1,
+        alertProviderProps,
+      );
+    });
   });
 
   it("tests onClickChangeProfile", async () => {
-    renderProfilePage(
-      profilePageUsersStub[0],
-      friendListStub,
-      currentProfileStub,
-      alertProviderProps,
-    );
+    await act(async () => {
+      renderProfilePage(
+        usersStub[0],
+        friendListStub,
+        currentProfileStub,
+        1,
+        alertProviderProps,
+      );
+    });
     const button = screen.getByText("프로필 수정");
     fireEvent.click(button);
   });
 
   it("tests onClickChatWithUser", async () => {
-    renderProfilePage(
-      profilePageUsersStub[1],
-      friendListStub,
-      currentProfileStub,
-      alertProviderProps,
-    );
+    await act(async () => {
+      renderProfilePage(
+        usersStub[0],
+        friendListStub,
+        currentProfileStub,
+        8,
+        alertProviderProps,
+      );
+    });
     const button = screen.getByText("채팅하기");
     fireEvent.click(button);
   });
 
   it("tests onClickEvaluateQuality", async () => {
-    renderProfilePage(
-      profilePageUsersStub[1],
-      friendListStub,
-      currentProfileStub,
-      alertProviderProps,
-    );
+    await act(async () => {
+      renderProfilePage(
+        usersStub[0],
+        friendListStub,
+        currentProfileStub,
+        8,
+        alertProviderProps,
+      );
+    });
     const button = screen.getByText("동료로서 평가하기");
     fireEvent.click(button);
   });
 
   it("tests onClickAddFriend success", async () => {
-    mockDispatch.mockReturnValue({
-      unwrap: () => {},
+    await act(async () => {
+      renderProfilePage(
+        usersStub[0],
+        friendListStub,
+        currentProfileStub,
+        9,
+        alertProviderProps,
+      );
     });
-    renderProfilePage(
-      profilePageUsersStub[1],
-      friendListStub2,
-      currentProfileStub,
-      alertProviderProps,
-    );
     const button = screen.getByText("친구 추가하기");
     fireEvent.click(button);
   });
@@ -232,27 +160,46 @@ describe("<ProfilePage/>", () => {
     mockDispatch.mockReturnValue({
       unwrap: () => Promise.reject({}),
     });
-    renderProfilePage(
-      profilePageUsersStub[1],
-      friendListStub2,
-      currentProfileStub,
-      alertProviderProps,
-    );
+    await act(async () => {
+      renderProfilePage(
+        usersStub[0],
+        friendListStub,
+        currentProfileStub,
+        9,
+        alertProviderProps,
+      );
+    });
     const button = screen.getByText("친구 추가하기");
     fireEvent.click(button);
   });
 
   it("tests getFriendProfileDataNoStateUpdate", async () => {
     mockDispatch.mockReturnValue({
-      unwrap: () => ({ ...profileStub }),
+      unwrap: () => Promise.reject({}),
     });
-    renderProfilePage(
-      profilePageUsersStub[0],
-      friendListStub3,
-      currentProfileStub,
-      alertProviderProps,
-    );
+    await act(async () => {
+      renderProfilePage(
+        usersStub[0],
+        friendListStub,
+        currentProfileStub,
+        8,
+        alertProviderProps,
+      );
+    });
   });
+
+  it("tests uncovered branches", async () => {
+    await act(async () => {
+      renderProfilePage(
+        usersStub[0],
+        friendListStub,
+        currentProfileStub,
+        4,
+        alertProviderProps,
+      );
+    });
+  });
+
   it("tests alert callback", async () => {
     mockDispatch.mockReturnValue({
       unwrap: () => Promise.reject({}),
@@ -275,6 +222,7 @@ describe("<ProfilePage/>", () => {
     const modalButton = screen.getByRole("button", { name: "확인" });
     fireEvent.click(modalButton);
   });
+
   it("tests null useParam userID", async () => {
     render(
       <Provider store={store}>
