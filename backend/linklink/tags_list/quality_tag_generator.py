3885d4f8bd5a7694b009fa7b28b1654afe1068aa
notiona <notiona@snu.ac.kr>
2022-12-02 02:09:58 +0900

    backend:fixture: change all qualityTags to Korean

--- backend/linklink/tags_list/quality_tag_generator.py
+++ backend/linklink/tags_list/quality_tag_generator.py
@@ -5,10 +5,10 @@ based on tags_list/tags.py
 
 import json # pragma: no cover
 
-import tags_list.tags as tags # pragma: no cover
+from tags import QUALITY_TAGS # pragma: no cover
 
 if __name__=="__main__": # pragma: no cover
-    quality_tags = tags.QUALITY_TAGS
+    quality_tags = QUALITY_TAGS
     quality_tags_json_fixture = []
     for idx, quality_tag in enumerate(quality_tags):
         quality_tag_json = {
@@ -19,5 +19,5 @@ if __name__=="__main__": # pragma: no cover
             }
         }
         quality_tags_json_fixture.append(quality_tag_json)
-    with open("init_fake_quality_tags.json", "w") as json_file:
+    with open("../fixtures/init_fake_quality_tags.json", "w") as json_file:
         json.dump(quality_tags_json_fixture, json_file, indent=4)
