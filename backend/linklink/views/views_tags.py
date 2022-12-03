ad6eff3629ed10389a69c8619d3ae670992344f4
notiona <notiona@snu.ac.kr>
2022-12-03 21:05:44 +0900

    backend: implement PUT /api/qualityTag/:userId

--- backend/linklink/views/views_tags.py
+++ backend/linklink/views/views_tags.py
@@ -2,7 +2,11 @@
 SkillTag, QualityTag related views module for linklink app
 """
 
+import json
+from json.decoder import JSONDecodeError
+
 from django.http import (
+    HttpResponseBadRequest,
     JsonResponse
 )
 
@@ -14,6 +18,7 @@ from ..models import (
     QualityTagRequest,
 )
 from ..utils import is_onechon
+from ..tags_list.tags import QUALITY_TAGS
 
 #--------------------------------------------------------------------------
 #   SkillTag, QualityTag Related APIs
@@ -50,14 +55,13 @@ def quality_tag(request, user_id):
         if LinkLinkUser.objects.filter(pk=user_id).exists():
             current_linklinkuser = request.user.linklinkuser
             linklinkuser = LinkLinkUser.objects.get(pk=user_id)
-            if is_onechon(current_linklinkuser, linklinkuser):
+            if is_onechon(current_linklinkuser, linklinkuser) and \
+                current_linklinkuser.id != linklinkuser.id:
                 # Get QualityTagRequest that are active,
                 # from current user && to linklinkuser
                 active_quality_tag_requests = QualityTagRequest.objects.filter(
-                    senderId=current_linklinkuser
-                ).filter(
-                    getterId=linklinkuser
-                ).filter(
+                    senderId=current_linklinkuser,
+                    getterId=linklinkuser,
                     status=True
                 )
                 response_dict = {"qualityTags": []}
@@ -84,6 +88,85 @@ def quality_tag(request, user_id):
                     f"userId={user_id} not found."
                 }
             )
-
     elif request.method == "PUT": # pragma: no branch
-        pass
+        if LinkLinkUser.objects.filter(pk=user_id).exists():
+            current_linklinkuser = request.user.linklinkuser
+            linklinkuser = LinkLinkUser.objects.get(pk=user_id)
+            if is_onechon(current_linklinkuser, linklinkuser) and \
+                current_linklinkuser.id != linklinkuser.id:
+                try:
+                    req_data = json.loads(request.body.decode())
+                    new_quality_tag_requests = req_data["qualityTags"]
+                except (KeyError, JSONDecodeError) as e:
+                    return HttpResponseBadRequest(e)
+                new_quality_tag_requests = [
+                    req["name"] for req in new_quality_tag_requests
+                ]
+                # Get QualityTagRequest that are active,
+                # from current user && to linklinkuser
+                old_quality_tag_requests = QualityTagRequest.objects.filter(
+                    senderId=current_linklinkuser,
+                    getterId=linklinkuser,
+                    status=True
+                )
+                old_quality_tag_requests = [
+                    req.name for req in old_quality_tag_requests
+                ]
+                # QualityTagRequest reset logic
+                # New - Old   : must either create or set to true
+                # Old - New   : must set to false
+                # New AND Old : leave them as true
+                new_set = set(new_quality_tag_requests)
+                old_set = set(old_quality_tag_requests)
+                new_minus_old_set = new_set.difference(old_set)
+                old_minus_new_set = old_set.difference(new_set)
+                for new_minus_old_quality_tag in new_minus_old_set:
+                    if new_minus_old_quality_tag in QUALITY_TAGS:
+                        true_quality_tag, _ = \
+                            QualityTagRequest.objects.get_or_create(
+                                senderId=current_linklinkuser,
+                                getterId=linklinkuser,
+                                name=new_minus_old_quality_tag
+                            )
+                        true_quality_tag.status = True
+                        true_quality_tag.save()
+                    else:
+                        return JsonResponse(
+                            status=400,
+                            data={
+                                "message":
+                                (
+                                    "Request includes invalid qualityTag: "
+                                    f"{new_minus_old_quality_tag}"
+                                )
+                            }
+                        )
+                for old_minus_new_quality_tag in old_minus_new_set:
+                    false_quality_tag = QualityTagRequest.objects.get(
+                        senderId=current_linklinkuser,
+                        getterId=linklinkuser,
+                        status=True,
+                        name=old_minus_new_quality_tag
+                    )
+                    false_quality_tag.status = False
+                    false_quality_tag.save()
+                return JsonResponse(
+                    status=200,
+                    data=req_data
+                )
+            else:
+                return JsonResponse(
+                    status=403,
+                    data={
+                        "message":
+                        f"No read permission for userId={user_id}."
+                    }
+                )
+        else:
+            return JsonResponse(
+                status=404,
+                data={
+                    "message":
+                    f"userId={user_id} not found."
+                }
+            )
