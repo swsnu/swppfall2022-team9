63bff22db7cdd8a81fbca768bcc2297bc37f5f57
dongjoocha <dongjoocha@naver.com>
2022-12-04 00:23:35 +0900

    Add asgi settings and routings for websocket

---
+++ backend/linklink/routing.py
@@ -0,0 +1,8 @@
+# chat/routing.py
+from django.urls import path
+
+from . import consumers
+
+websocket_urlpatterns = [
+    path("<chat_room_name>/", consumers.ChatConsumer.as_asgi()),  # user1_id < user2_id
+]
