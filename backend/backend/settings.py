fdfda22eb3c72f4e4f4fdc258253bd1778f4fd8f
Kim Dong Hun <hunkim98@gmail.com>
2022-12-03 23:46:52 +0900

    Change domain settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -34,7 +34,8 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "0.0.0.0", "localhost", "15.165.89.185"]
+ALLOWED_HOSTS = ["127.0.0.1", "0.0.0.0",
+                 "localhost", "15.165.89.185", "hoshiwoobo.shop"]
 
 
 # Application definition
