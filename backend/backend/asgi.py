121d77a112d699e5f92828dca4d3c5d2dc46e41a
Kim Dong Hun <hunkim98@gmail.com>
2022-12-05 23:19:57 +0900

    Change import order

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -10,19 +10,15 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 
+from django.core.asgi import get_asgi_application
+django_asgi_app = get_asgi_application()
+
 import linklink.routing
 from channels.security.websocket import AllowedHostsOriginValidator
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from django.core.asgi import get_asgi_application
-
-# @formatter:off
 
 
-# @formatter:on
-
-
-django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter(
     {
