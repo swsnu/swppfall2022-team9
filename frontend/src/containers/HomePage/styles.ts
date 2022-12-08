234b64bd44340311227fbf268fea1aeecab413c9
dongjoocha <dongjoocha@naver.com>
2022-12-08 19:10:24 +0900

    Remove unused style

--- frontend/src/containers/HomePage/styles.ts
+++ frontend/src/containers/HomePage/styles.ts
@@ -7,13 +7,3 @@ export const Container = styled.div`
   min-height: 100vh;
   position: fixed;
 `;
-
-export const GraphContainer = styled.div`
-  background: linear-gradient(
-    180deg,
-    rgba(252, 100, 255, 0.2) 0%,
-    rgba(121, 154, 237, 0.116) 100%
-  );
-  flex: 1 0;
-  overflow: hidden;
-`;
