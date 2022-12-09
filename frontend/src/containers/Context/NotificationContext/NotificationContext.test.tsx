040c9768bb4f42bf9c999248d281c6e1656c4f3e
dongjoocha <dongjoocha@naver.com>
2022-12-10 04:50:02 +0900

    Add NotificationContext test

--- frontend/src/containers/Context/NotificationContext/NotificationContext.test.tsx
+++ frontend/src/containers/Context/NotificationContext/NotificationContext.test.tsx
@@ -1,7 +1,14 @@
+import { render } from "@testing-library/react";
+import { WS } from "jest-websocket-mock";
 import { useContext } from "react";
-import { MemoryRouter, Route, Routes } from "react-router-dom";
-import { renderWithProviders } from "test-utils/mocks";
-import { NotificationContext } from "./NotificationContext";
+import { act } from "react-dom/test-utils";
+import { Provider } from "react-redux";
+import { usersStub } from "server/stubs/users.stub";
+import { setupStore } from "store/slices";
+import {
+  NotificationContext,
+  NotificationContextProvider,
+} from "./NotificationContext";
 
 const CustomTest = () => {
   //default is set to 0
@@ -14,21 +21,34 @@ const CustomTest = () => {
   );
 };
 
-const renderNotificationTest = () => {
-  renderWithProviders(
-    <MemoryRouter>
-      <Routes>
-        <Route path="/" element={<CustomTest />} />
-      </Routes>
-    </MemoryRouter>,
-    {
-      preloadedState: {},
-    },
-  );
-};
-
 describe("NotificationContext", () => {
   it("render notification context", async () => {
-    renderNotificationTest();
+    const server = new WS("wss://hoshiwoobo.shop:8001/ws/notification/1/", {
+      jsonProtocol: true,
+    });
+
+    await act(async () => {
+      render(
+        <Provider
+          store={setupStore({
+            users: { currentUser: usersStub[0], friendList: [] },
+          })}
+        >
+          <NotificationContextProvider>
+            <CustomTest />
+          </NotificationContextProvider>
+        </Provider>,
+      );
+    });
+
+    server.send({
+      type: "new_message_notification",
+      senderId: 1,
+      content: "hello",
+      timeStamp: "2022-12-04T16:57:37.039Z",
+    });
+    server.send({ type: "unread_count", messages: 10 });
+    server.send({ type: "unknown_type" });
+    server.close();
   });
 });
