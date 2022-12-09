8c0b2ef8dd6668e020263329ff08674f3f4518bc
notiona <notiona@snu.ac.kr>
2022-12-09 18:39:21 +0900

    backend:lint: lint chat related python files

--- backend/linklink/routing.py
+++ backend/linklink/routing.py
@@ -6,8 +6,12 @@ from django.urls import path
 from linklink import consumers
 
 websocket_urlpatterns = [
-    path("ws/notification/<user_id>/", consumers.NotificationConsumer.as_asgi()),
     path(
-        "ws/<chat_room_name>/", consumers.ChatConsumer.as_asgi()  # user1_id < user2_id
+        "ws/notification/<user_id>/",
+        consumers.NotificationConsumer.as_asgi()
+    ),
+    # user1_id < user2_id
+    path(
+        "ws/<chat_room_name>/", consumers.ChatConsumer.as_asgi()
     ),
 ]
