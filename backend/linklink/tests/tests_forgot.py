abac101c06cb21df599a62cb2375a981c33c7aba
notiona <notiona@snu.ac.kr>
2022-12-10 19:40:57 +0900

    backend:test: check GET /api/forgot/username/

---
+++ backend/linklink/tests/tests_forgot.py
@@ -0,0 +1,178 @@
+"""
+Forgot related test module for linklink app
+"""
+
+from datetime import datetime, timedelta
+import json
+import os
+
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.core import mail
+from django.test import TestCase, Client
+from django.utils import timezone
+
+from ..models import (
+    LinkLinkUser,
+    FriendRequest,
+    Verification,
+    SkillTag,
+    QualityTag,
+    Profile,
+    Education,
+    JobExperience,
+    QualityTagRequest,
+)
+
+
+class LinkLinkForgotTestCase(TestCase):
+    """
+    Forgot related TestCase to test various LinkLink REST APIs
+    """
+    def setUp(self):
+        # Create 5 Users
+        john = User.objects.create_user(
+            username="john",
+            password="johnpassword",
+            #email="notiona@snu.ac.kr",
+            first_name="John",
+            last_name="Cena"
+        )
+        james = User.objects.create_user(
+            username="james",
+            password="jamespassword",
+            #email="notiona@snu.ac.kr",
+            first_name="James",
+            last_name="Gunn"
+        )
+        emily = User.objects.create_user(
+            username="emily",
+            password="emilypassword",
+            #email="notiona@snu.ac.kr",
+            first_name="Emily",
+            last_name="Blunt"
+        )
+        will = User.objects.create_user(
+            username="will",
+            password="willpassword",
+            #email="notiona@snu.ac.kr",
+            first_name="Will",
+            last_name="Smith"
+        )
+        chris = User.objects.create_user(
+            username="chris",
+            password="chrispassword",
+            #email="notiona@snu.ac.kr",
+            first_name="Chris",
+            last_name="Rock"
+        )
+        john_linklinkuser = LinkLinkUser.objects.create(
+            user=john,
+            emailValidated=False,
+            email_unique="notiona@snu.ac.kr",
+        )
+        james_linklinkuser = LinkLinkUser.objects.create(
+            user=james,
+            emailValidated=False,
+            email_unique="invalid_but_unique1@snu.ac.kr",
+        )
+        emily_linklinkuser = LinkLinkUser.objects.create(
+            user=emily,
+            emailValidated=False,
+            email_unique="invalid_but_unique2@snu.ac.kr",
+        )
+        will_linklinkuser = LinkLinkUser.objects.create(
+            user=will,
+            emailValidated=False,
+            email_unique="invalid_but_unique3@snu.ac.kr",
+        )
+        chris_linklinkuser = LinkLinkUser.objects.create(
+            user=chris,
+            emailValidated=False,
+            email_unique="invalid_but_unique4@snu.ac.kr",
+        )
+        expire_time = \
+            datetime.now() + timedelta(days=settings.EMAIL_EXPIRE_DAYS)
+        expire_time= expire_time.astimezone(timezone.get_default_timezone())
+        Verification.objects.create(
+            linklinkuser=john_linklinkuser,
+            purpose="Register",
+            expiresAt=expire_time
+        )
+        Verification.objects.create(
+            linklinkuser=james_linklinkuser,
+            purpose="Register",
+            expiresAt=expire_time
+        )
+        Verification.objects.create(
+            linklinkuser=emily_linklinkuser,
+            purpose="Register",
+            expiresAt=expire_time
+        )
+        Verification.objects.create(
+            linklinkuser=will_linklinkuser,
+            purpose="Register",
+            expiresAt=expire_time
+        )
+        Verification.objects.create(
+            linklinkuser=chris_linklinkuser,
+            purpose="Register",
+            expiresAt=expire_time
+        )
+        SkillTag.objects.create(
+            name="Frontend"
+        )
+        SkillTag.objects.create(
+            name="Backend"
+        )
+        SkillTag.objects.create(
+            name="DevOps"
+        )
+        QualityTag.objects.create(
+            name="성실한"
+        )
+        QualityTag.objects.create(
+            name="정직한"
+        )
+        QualityTag.objects.create(
+            name="논리적인"
+        )
+        # Initialize frequently used member variables
+        self.client = Client(enforce_csrf_checks=True)
+        self.csrftoken = \
+            self.client.get("/api/csrf_token/").cookies["csrftoken"].value
+        self.linklink_path = os.path.dirname(os.path.realpath(__file__))
+
+#--------------------------------------------------------------------------
+#   Auth Related Tests
+#--------------------------------------------------------------------------
+
+    def test_get_forgot_username_success(self):
+        target_url = "/api/forgot/username/notiona@snu.ac.kr/"
+        # GET
+        response = self.client.get(target_url)
+        self.assertEqual(response.status_code, 200)
+        # Check response
+        expected_response_dict = {
+            "username": "john"
+        }
+        self.assertDictEqual(
+            json.loads(response.content.decode()),
+            expected_response_dict
+        )
+
+
+    def test_404_get_forgot_username(self):
+        target_url = "/api/forgot/username/doesnotexist@snu.ac.kr/"
+        # GET
+        response = self.client.get(target_url)
+        self.assertEqual(response.status_code, 404)
+        # Check response
+        error_message_dict = {
+            "message": "user not found for given email doesnotexist@snu.ac.kr"
+        }
+        self.assertDictEqual(
+            json.loads(response.content.decode()),
+            error_message_dict
+        )
+
