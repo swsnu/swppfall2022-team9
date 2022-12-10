cabccc94db11306ec852878c6938340dc51b93f6
notiona <notiona@snu.ac.kr>
2022-12-10 15:46:30 +0900

    backend: implement GET /api/forgot/username/

--- backend/linklink/views/views_forgot.py
+++ backend/linklink/views/views_forgot.py
@@ -5,6 +5,7 @@ Forgot related views module for linklink app
 import json
 from json.decoder import JSONDecodeError
 
+from django.contrib.auth.models import User
 from django.http import (
     HttpResponseBadRequest,
     JsonResponse
@@ -18,7 +19,6 @@ from ..models import (
     QualityTagRequest,
 )
 from ..utils import is_onechon
-from ..tags_list.tags import QUALITY_TAGS
 
 #--------------------------------------------------------------------------
 #   Forgot Related APIs
@@ -27,7 +27,23 @@ from ..tags_list.tags import QUALITY_TAGS
 @allowed_method_or_405(["GET"])
 def forgot_username(request):
     if request.method == "GET": # pragma: no branch
-        pass
+        try:
+            req_data = json.loads(request.body.decode())
+            email = req_data["email"]
+        except (KeyError, JSONDecodeError) as e:
+            return HttpResponseBadRequest(e) # implicit status code = 400
+        try:
+            linklinkuser_found = LinkLinkUser.objects.get(email_unique=email)
+        except LinkLinkUser.DoesNotExist:
+            return JsonResponse(
+                status=404,
+                data={"message": f"user not found for given email {email}"}
+            )
+        username_found = linklinkuser_found.user.username
+        return JsonResponse(
+            status=200,
+            data={"username":username_found}
+        )
 
 
 @allowed_method_or_405(["POST, PUT"])
