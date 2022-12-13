f9a461aa0efba9671387d6e9e348465b598d27dc
dongjoocha <dongjoocha@naver.com>
2022-12-13 23:16:44 +0900

    Fix timeStamp to display timeStamp before yesterday

--- frontend/src/utils/timeStamp.ts
+++ frontend/src/utils/timeStamp.ts
@@ -12,8 +12,44 @@ export const compareTimeStampWtihinDay = (
   return hourA === hourB && minuteA === minuteB;
 };
 
+const monthWith31Days = [1, 3, 5, 7, 8, 10, 12];
+const monthWith30Days = [4, 6, 9, 11];
+
 export const timeStampToString = (timestamp: string): string => {
   const date = new Date(timestamp);
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1; // January is 0
+  const day = date.getDate();
+
+  const now = new Date();
+  const currentYear = now.getFullYear();
+  const currentMonth = now.getMonth() + 1; // January is 0
+  const currentDay = now.getDate();
+
+  if (year !== currentYear) {
+    if (
+      currentYear - year === 1 &&
+      month === 12 &&
+      day === 31 &&
+      currentMonth === 1 &&
+      currentDay === 1
+    )
+      return "어제";
+    else return `${month}월 ${day}일`;
+  }
+  if (month !== currentMonth) {
+    if (
+      currentMonth - month === 1 &&
+      currentDay === 1 &&
+      ((monthWith31Days.find(m => m === month) && day === 31) ||
+        (monthWith30Days.find(m => m === month) && day === 30) ||
+        (month === 2 && (day === 28 || day === 29)))
+    )
+      return "어제";
+    else return `${month}월 ${day}일`;
+  }
+  if (currentDay - day === 1) return "어제";
+
   const hour = date.getHours();
   const minutes = date.getMinutes();
   const isAM = hour < 12;
